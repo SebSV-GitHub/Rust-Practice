2056be453538cc623fd5a6590e98538ee25c655a
Sebastian Suarez <apogees-miscue-0t@icloud.com>
2023-03-01 20:08:51 -0500

    Completed section

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3; // Constants
+
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    println!("{x}!");
+    x = 6; // Variable mutation, wont work without mut
+    println!("{x}!");
+    println!("{THREE_HOURS_IN_SECONDS}!");
+
+    // Shadowing
+    let y = 4;
+    let y = y + 1; // Old y still exist but the name y changed its location
+
+    {
+        let y = y * 2; // Here the y variable overshadows the old variable taking it name, this
+                       // will end when the block scope ends
+        println!("{y}")
+    }
+    println!("{y}");
+
+    let y = "Hello, world!";
+
+    println!(y); // Shadowing also lets you change the variable type
 }
