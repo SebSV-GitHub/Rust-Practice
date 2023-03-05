9ebe849ee596803f89c7b2fb149a167ad195a9a5
Sebastian Suarez <apogees-miscue-0t@icloud.com>
2023-03-05 11:58:50 -0500

    Completed section

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+// Functions
+
 fn main() {
     println!("Hello, world!");
+    another_function(3);
+}
+
+fn another_function(x: i32) {
+    let y = five();
+    let y = plus_one(y);
+    println!("Other function invoked with x: {x}, y: {y}");
+}
+
+fn five() -> i32 {
+    // This function uses the expression 5 (without the semicolon) to return that
+    // number
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
